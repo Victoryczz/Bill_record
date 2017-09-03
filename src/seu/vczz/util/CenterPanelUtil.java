fbcbc0a463aa8a11437ab461458505d5cd34cbc0
Victoryczz <vchenzz93@gmail.com>
2017-09-03 22:42:40 +0800

    Util

--- src/seu/vczz/util/CenterPanelUtil.java
+++ src/seu/vczz/util/CenterPanelUtil.java
@@ -1,11 +1,12 @@
 package seu.vczz.util;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * 控件居中util
  */
-public class CenterPanelUtil {
+public class CenterPanelUtil extends JPanel{
     private double rate;//拉伸比例
     private JComponent component;//显示拉伸组件
     private boolean strech;//是否拉伸
@@ -16,5 +17,35 @@ public class CenterPanelUtil {
     }
     public CenterPanelUtil(double rate){
         this(rate, true);//调用上一个构造函数
+    }
+
+    @Override
+    public void repaint() {
+        if (component != null){
+            Dimension containerSize = this.getSize();//尺寸
+            Dimension componentSize = component.getSize();
+
+            if (strech){
+                component.setSize( (int)(containerSize.width*rate), (int) (containerSize.height*rate));
+            }else {
+                component.setSize(componentSize);
+            }
+            //矩形居中
+            component.setLocation(containerSize.width / 2 - component.getSize().width / 2, containerSize.height / 2 - component.getSize().height / 2);
+        }
+        super.repaint();
+    }
+    @Override
+    public void show(JComponent component){
+        this.component = component;
+        Component[] cs = getComponents();
+        for(Component c : cs){
+            remove(c);
+        }
+        add(component);
+        if (component is WorkingPanel){
+            ((WorkingPanel)component).updateData();
+        }
+        this.updateUI();
     }
 }
